26c791e53cc058b99941c04b5ec76535c9f76c82
Datta <2096688@cognizant.com>
2022-06-15 21:03:25 +0530

    added New Expense form UI

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Expenses from "./Components/Expenses/Expenses";
+import NewExpense from "./Components/NewExpense/NewExpense";
 
 function App() {
   const expenses = [
@@ -8,6 +9,7 @@ function App() {
   return (
     <div>
       <h2>Expense Tracker</h2>
+      <NewExpense></NewExpense>
       <Expenses items={expenses}/>
     </div>
   );
