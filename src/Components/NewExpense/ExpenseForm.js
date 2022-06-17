6e7b32fe19c25305c41cdcbc1b6ef0d6bb6910df
Datta <2096688@cognizant.com>
2022-06-17 07:52:58 +0530

    added two way binding,i.e clearing form fields on submit

--- src/Components/NewExpense/ExpenseForm.js
+++ src/Components/NewExpense/ExpenseForm.js
@@ -30,17 +30,24 @@ const ExpenseForm = () => {
     });
   };
 
+  const submitHandler = (event) => {
+    event.preventDefault()
+    console.log(userInput)
+    setUserInput({enteredTitle:'', enteredAmount: '', enteredDate: ''})
+  }
+
   return (
-    <form>
+    <form onSubmit={submitHandler}>
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
-          <input type="text" onChange={titleChangeHandler} />
+          <input type="text" value={userInput.enteredTitle} onChange={titleChangeHandler} />
         </div>
         <div className="new-expense__control">
           <label>Amount</label>
           <input
             type="number"
+            value={userInput.enteredAmount}
             min="0.01"
             step="0.01"
             onChange={amountChangeHandler}
@@ -49,8 +56,9 @@ const ExpenseForm = () => {
         <div className="new-expense__control">
           <label>Date</label>
           <input
-            type="date"
-            min="2029-01-01"
+            type="Date"
+            value={userInput.enteredDate}
+            min="2019-01-01"
             max="2022-12-31"
             onChange={dateChangeHandler}
           />
