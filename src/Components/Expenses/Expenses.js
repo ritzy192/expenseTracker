2b3fd1c7be89aa5d2abcefffaf14abcf70f0d75d
Datta <2096688@cognizant.com>
2022-06-18 08:34:33 +0530

    addded expenseFilter compoent

--- src/Components/Expenses/Expenses.js
+++ src/Components/Expenses/Expenses.js
@@ -1,22 +1,31 @@
+import {useState} from 'react'
 import Card from "../UI/Card";
 import ExpenseItem from "./ExpenseItem";
-import './Expenses.css'
+import "./Expenses.css";
+import ExpensesFilter from "./ExpensesFilter";
 
 const Expenses = (props) => {
+  const [filterYear, setFilterYear] = useState('2022')
+  const filterChangeHandler = (selectedYear) => {
+    setFilterYear(selectedYear);
+  };
   return (
-    <Card className="expenses">
-      <ExpenseItem
-        title={props.items[0].title}
-        price={props.items[0].price}
-        date={props.items[0].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={props.items[1].title}
-        price={props.items[1].price}
-        date={props.items[1].date}
-      ></ExpenseItem>
-    </Card>
+    <div>
+      <Card className="expenses">
+        <ExpensesFilter yearSelected={filterYear} onFilterChange={filterChangeHandler} />
+        <ExpenseItem
+          title={props.items[0].title}
+          price={props.items[0].price}
+          date={props.items[0].date}
+        ></ExpenseItem>
+        <ExpenseItem
+          title={props.items[1].title}
+          price={props.items[1].price}
+          date={props.items[1].date}
+        ></ExpenseItem>
+      </Card>
+    </div>
   );
-}
+};
 
 export default Expenses;
