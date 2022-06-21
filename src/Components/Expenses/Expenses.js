087df61a7051c6632478fbcc287ff120631df96a
Datta <2096688@cognizant.com>
2022-06-21 17:55:42 +0530

    merged changes

--- src/Components/Expenses/Expenses.js
+++ src/Components/Expenses/Expenses.js
@@ -1,30 +1,29 @@
 import { useState } from "react";
 import Card from "../UI/Card";
-import ExpenseItem from "./ExpenseItem";
 import "./Expenses.css";
+import ExpensesChart from "./ExpensesChart";
 import ExpensesFilter from "./ExpensesFilter";
+import FilteredExpenses from "./FilteredExpenses";
 
 const Expenses = (props) => {
   const [filterYear, setFilterYear] = useState("2022");
   const filterChangeHandler = (selectedYear) => {
     setFilterYear(selectedYear);
   };
-  props.items.map((expense) => console.log(expense));
-  return (
+  const expensesByYear = props.items.filter((expense) => {
+    return expense.date.getFullYear()===parseInt(filterYear)
+  });
+  console.log('expenses by year')
+  console.log(expensesByYear) 
+  return (  
     <div>
       <Card className="expenses">
         <ExpensesFilter
           yearSelected={filterYear}
           onFilterChange={filterChangeHandler}
         />
-        {props.items.map((expense) => (
-          <ExpenseItem
-            key={expense.id}
-            title={expense.title}
-            price={expense.price}
-            date={expense.date}
-          />
-        ))}
+        <ExpensesChart expenses={expensesByYear}/>
+        <FilteredExpenses items={expensesByYear}/>
       </Card>
     </div>
   );
